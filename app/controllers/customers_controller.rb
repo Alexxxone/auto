7b3186127176a797ffc28d6933966bc9961e3777
Alex <bulletone@mail.ru>
2014-06-02 15:52:59 +0300

    add new freatures

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,6 @@
 class CustomersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_customer, only: [:show,:edit, :update, :destroy]
+  before_action :set_customer, only: [:show,:edit,  :update, :destroy]
 
   def index
     @customers = Customer.order('created_at DESC').includes(:user, :payments)
@@ -8,6 +8,35 @@ class CustomersController < ApplicationController
   def show
 
   end
+  def new
+     @customer = current_user.customers.new
+  end
+  def create
+    @customer = Customer.new(customer_params)
+    @customer.user_id = current_user.id
+    respond_to do |format|
+      if @customer.save
+        format.html { redirect_to  @customer, notice: 'Заказчик успешно создан!' }
+        format.json { render action: 'show', status: :created, location: @customer }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @customer.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @customer.update(customer_params)
+        format.html { redirect_to @customer, notice: 'Заказчик успешно обновлен!' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @customer.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 
   def find_number_code
     q = params[:number_code]
@@ -15,7 +44,10 @@ class CustomersController < ApplicationController
     render json: customers
   end
   private
-  def set_customer
-    @customer = Customer.find(params[:id])
-  end
+    def set_customer
+      @customer = Customer.find(params[:id])
+    end
+    def customer_params
+      params.require(:customer).permit(:name, :number_code)
+    end
 end
